7a60a8f34ba17d5dd0e8d25af6b72e75d00195d8
Sean Nam <skyun.nam@gmail.com>
2020-08-06 11:49:02 +0900

    비트 연산, 3항연산 연습 연산자 마무리

--- JavaEx/src/com/javaex/basic/OperEx.java
+++ JavaEx/src/com/javaex/basic/OperEx.java
@@ -6,7 +6,65 @@ public class OperEx {
 	public static void main(String[] args) {
 		// arithOper();
 //		incDecOper();
-		logicalOper();
+//		logicalOper();
+//		bitOper();
+		conditionalOper();
+	}
+	
+	//	3항 연산자
+	public static void conditionalOper() {
+		//	논리비교 ? 참일때 : 거짓일때
+		//	간단한 값의 제어, 코드 가독성을 높일 수 있다
+		int a = 10;
+		//	a가 짝수면 "짝수", 아니면 "홀수" 문자열
+		String message = a % 2 == 0 ? "짝수" : "홀수";
+		System.out.println("a는 짝수입니까? " + message);
+		
+		int score = 70;
+		//	점수가 80 이상이면 Good
+		//	점수가 50점 이상이면 Pass
+		//	그렇지 않으면 Fail
+		message = score >= 80 ? "Good" :
+			score >= 50 ? "Pass" : "Fail";
+		System.out.println("시험 결과:" + message);
+		//	중첩 가능하긴 하지만 가독성이 떨어지는 문제
+		//	복잡한 비교의 경우는 비교문으로 해결
+	}
+	
+	//	비트 연산자
+	public static void bitOper() {
+		//	and (&), or (|), not (~)
+		//	정수형 데이터를 비트 단위로 제어하는 연산자
+		byte b1 = 0b1101;
+		byte b2 = 0b0111;
+		
+		int result = b1 & b2; // bit and 연산
+		System.out.println(
+				Integer.toBinaryString(result));
+		
+		result = b1 | b2; // bit or 연산
+		System.out.println(
+				Integer.toBinaryString(result));
+		
+		result = ~b1;	//	bit not 연산 0 <-> 1
+		System.out.println(
+				Integer.toBinaryString(result));
+		System.out.println(result);
+		
+		//	비트 시프트
+		//		<< : 비트를 왼쪽으로 이동 (정수형 2의 배수의 곱)
+		//		>> : 비트를 오른쪽으로 이동 (정수형 2의 배수의 나눗셈)
+		
+		int val = 1;
+		System.out.println(val);
+		//	왼쪽으로 1비트 이동
+		System.out.println(
+				Integer.toBinaryString(val << 1));
+		
+		val = 0b100;
+		//	오른쪽으로 2비트 이동
+		System.out.println(
+				Integer.toBinaryString(val >> 2));
 	}
 	
 	//	비교, 논리 연산
