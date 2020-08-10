2670d919123e720369772417888e9052c6787cfe
Sean Nam <skyun.nam@gmail.com>
2020-08-10 11:56:07 +0900

    While 반복문

--- JavaEx/src/com/javaex/basic/ConditionalEx.java
+++ JavaEx/src/com/javaex/basic/ConditionalEx.java
@@ -7,7 +7,134 @@ public class ConditionalEx {
 	public static void main(String[] args) {
 //		ifElseEx();
 //		ifElseEx2();
-		ifElseEx3();
+//		ifElseEx3();
+//		switchEx();
+//		switchEx2();
+//		whileEx();
+//		whileEx2();
+//		whileEx3();
+		doWhileEx();
+	}
+	
+	public static void doWhileEx() {
+		//	숫자를 입력 받아서 합산
+		//	0을 입력 받으면 반복을 종료
+		int num = 0;
+		int total = 0; //	합산 변수
+		
+		Scanner scanner = new Scanner(System.in);
+		
+		//	최소 1번을 실행해야 하는 반복 do ~ while
+		//	or 반복 조건이 loop 문 내부에서 결정되는 경우 do ~ while
+		do { //	일단 실행
+			System.out.print("정수입력.[0이면 종료]:");
+			num = scanner.nextInt();
+			total += num;
+		} while(num != 0);
+		
+		System.out.println("합산값:" + total);
+		scanner.close();
+	}
+	
+	public static void whileEx3() {
+		//	while은 종료를 위한 제어 변수를 잘 제어하여
+		//	무한루프에 빠지지 않도록 해야 한다
+		//	경우에 따라서는 의도적으로 무한루프를 사용하기도 한다
+		while(true) {	//	조건이 true면 항상 반복
+			System.out.println("무한루프... Ctrl+C to Quit");
+		}
+	}
+	
+	public static void whileEx2() {
+		//	단수를 입력, 해당 단의 곱셈표를 출력
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("단을 입력:");
+		int dan = scanner.nextInt();
+		//	제어용 변수
+		int i = 1;
+		
+		while(i <= 9) {	//	9이하일 동안 반복
+			System.out.println(dan + " * " + i + " = " + (dan * i));
+			//	종료를 위한 제어변수를 잘 제어
+			i++;
+		}
+		scanner.close();
+	}
+	
+	public static void whileEx() {
+		//	반복문을 사용, 1부터 100까지 숫자를 합산하여 출력
+		int num = 1;	//	시작값, 조건 비교
+		int total = 0;	//	합산 변수
+		
+		//	while loop
+		while (num <= 100) {	//	num가 100 이하인 동안 반복
+			total += num; 	// <- total = total + num
+			//	주의: while문은 반복 조건을 개발자가 직접 컨트롤 해 줘야 한다
+			num++;
+		}
+		System.out.println("합산값:" + total);
+	}
+	
+	public static void switchEx2() {
+		//	월정보(정수) 입력
+		//	1, 3, 5, 7, 8, 10, 12 -> 31일
+		//	2 -> 28일
+		//	4, 6, 9, 11 -> 30일 출력
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("월 입력:");
+		int month = scanner.nextInt();
+		String days = "";
+		
+		switch (month) {
+		case 1:
+		case 3:
+		case 5:
+		case 7:
+		case 8:
+		case 10:
+		case 12:
+			days = "31일";
+			break;
+		case 2:
+			days = "28일";
+			break;
+		case 4:
+		case 6:
+		case 9:
+		case 11:
+			days = "30일";
+			break;
+		default:
+			days = "?";
+		}
+		System.out.println(days);
+		scanner.close();
+	}
+	
+	public static void switchEx() {
+		//	ifElseEx3와 동일한 내용을 switch
+		//	switch는 전달받은 변수의 값을 이용해서 흐름을 제어 (==)
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("1:C, 2:C++, 3:Java, 4:Python -");
+		
+		int code = scanner.nextInt();
+		switch(code) {	//	switch는 변수의 값을 비교 흐름 제어 
+		case 1:	//	code == 1
+			System.out.println("R101");
+			break;	//	잊지 말자 break
+		case 2: //	code == 2
+			System.out.println("R202");
+			break;
+		case 3:
+			System.out.println("R303");
+			break;
+		case 4:
+			System.out.println("R404");
+			break;
+		default:	//	else 위에 case 문에서 일치하는 결과 없을 때
+			System.out.println("관리자에 문의");
+		}
+		scanner.close();
 	}
 	
 	public static void ifElseEx3() {
