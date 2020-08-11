6bcdeb7217e7858dcb0319baed352c9b7cfa4fad
Sean Nam <skyun.nam@gmail.com>
2020-08-11 10:13:00 +0900

    Math.random, 기본 문법 종료

--- JavaEx/src/com/javaex/basic/ConditionalEx.java
+++ JavaEx/src/com/javaex/basic/ConditionalEx.java
@@ -16,7 +16,24 @@ public class ConditionalEx {
 //		doWhileEx();
 //		forEx();
 //		continueEx();
-		breakEx();
+//		breakEx();
+		randomEx();
+	}
+	
+	public static void randomEx() {
+		//	 복수번의 랜덤 (1 ~ 6)을 뽑아서 합산
+		int rollCnt = 6; //	횟수
+		int total = 0;	// 합산변수
+		
+		for (int i = 1; i <= rollCnt; i++) {
+			//	랜덤으로 주사위 값을 추출
+			int num = (int)(Math.random() * 6) + 1;
+			System.out.println(i + "번째 주사위:" + num);
+			total += num;
+		}
+		
+		System.out.println("주사위 총합:" + total);
+		
 	}
 	
 	public static void breakEx() {
